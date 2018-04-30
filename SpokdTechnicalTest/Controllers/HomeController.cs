30527ebb5726041ce851a4b0dfe64ac84546f6d1
Lee Murphy <L.Murphy@cestrian.co.uk>
2018-04-30 14:40:59 +0100

    Add project files.

---
+++ SpokdTechnicalTest/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using SpokdTechnicalTest.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SpokdTechnicalTest.Controllers
+{
+    [EnableCors(origins: "http://localhost:54378", headers: "*", methods: "*")]
+    public class HomeController : ApiController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="targetNumber"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        public IHttpActionResult GetPrimeNumbers(int id)
+        {
+            var primeNumberViewModel = new PrimeNumberViewModel();
+
+            var primeNumbers = Enumerable.Range(1, Convert.ToInt32(Math.Floor(Math.Sqrt(id))))
+                .Aggregate(Enumerable.Range(1, id).ToList(),
+                    (result, index) =>
+                    {
+                        result.RemoveAll(i => i > result[index] && i % result[index] == 0);
+                        return result;
+                    }
+            );
+
+            primeNumberViewModel.TargetNumber = id;
+            primeNumberViewModel.PrimeNumbers = primeNumbers;
+
+            return Ok(primeNumberViewModel);
+        }
+
+        [AllowAnonymous]
+        public IHttpActionResult AltGetPrimeNumbers(int id)
+        {
+            var primeNumberViewModel = new PrimeNumberViewModel();
+
+            var primeNumbers = Enumerable.Range(1, (int)Math.Floor(2.52 * Math.Sqrt(id) / Math.Log(id))).Aggregate(
+                 Enumerable.Range(1, id).ToList(),
+                 (result, index) =>
+                 {
+                     var bp = result[index]; var sqr = bp * bp;
+                     result.RemoveAll(i => i >= sqr && i % bp == 0);
+                     return result;
+                 }
+             );
+
+            primeNumberViewModel.TargetNumber = id;
+            primeNumberViewModel.PrimeNumbers = primeNumbers;
+
+            return Ok(primeNumberViewModel);
+        }
+
+    }
+}
---
+++ SpokdTechnicalTest/ViewModels/PrimeNumberViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpokdTechnicalTest.ViewModels
+{
+    public class PrimeNumberViewModel
+    {
+        public int TargetNumber { get; set; }
+        public List<int> PrimeNumbers { get; set; }
+    }
+}
